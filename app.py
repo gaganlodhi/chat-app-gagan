fa9e1fdec9c1e9f63cdc2b3c4bfee6e14fd66e6b
gaganlodhi <gaganlodhi263@gmail.com>
2026-04-15 15:09:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,3 +23,14 @@ def handle_message(msg):
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=10000)
+    @socketio.on('message')
+def handle_message(data):
+    username = data['username']
+    message = data['message']
+
+    print(username + ": " + message)
+
+    send({
+        'username': username,
+        'message': message
+    }, broadcast=True)
