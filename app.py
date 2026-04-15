b9bca811ef0ffd70ced18ff8203c4cfaefd075f7
gaganlodhi <gaganlodhi263@gmail.com>
2026-04-15 17:19:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,17 @@
-import eventlet
-eventlet.monkey_patch()
-
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
-socketio = SocketIO(
-    app,
-    cors_allowed_origins="*",
-    async_mode="eventlet"
-)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@socketio.on("message")
-def handle_message(msg):
-    print("Received:", msg)
-    socketio.emit("message", msg)  # IMPORTANT FIX
-
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=10000)
-    @socketio.on('message')
+@socketio.on('message')
 def handle_message(data):
-    username = data['username']
-    message = data['message']
+    send(data, broadcast=True)
 
-    print(username + ": " + message)
-
-    send({
-        'username': username,
-        'message': message
-    }, broadcast=True)
+if __name__ == '__main__':
+    socketio.run(app, debug=True
