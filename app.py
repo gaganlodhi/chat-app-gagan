8d848b3b5b44ff7f0a126f1e79c70e733ac41071
gaganlodhi <gaganlodhi263@gmail.com>
2026-04-16 12:49:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+# store usernames temporarily
+users = {}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@socketio.on('join')
+def handle_join(username):
+    users[request.sid] = username
+    print(username + " joined")
+
 @socketio.on('message')
-def handle_message(data):
-    send(data, broadcast=True)
+def handle_message(msg):
+    username = users.get(request.sid, "Unknown")
+    full_msg = f"{username}: {msg}"
+    print(full_msg)
+    send(full_msg, broadcast=True)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    users.pop(request.sid, None)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
